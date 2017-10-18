136b4d43a08a56f3f5e070e0924cf2b7c0f78a45
bhatarchanas <archana@ncb-sv-016.ducom.edu>
2017-10-18 15:20:07 -0400

    Added comments

--- slack_dm_backup.rb
+++ slack_dm_backup.rb
@@ -1,6 +1,6 @@
 require 'trollop'
 
-#USAGE: ruby ../mcsmrt/get_fastqs.rb -s #{sample_key_file} -o #{all_bc_reads_output_folder} 
+#USAGE: ruby slack_dm_backup.rb -t token_from_slack -o output_dir
 
 ##### Input 
 opts = Trollop::options do
@@ -12,9 +12,11 @@ end
 opts[:token].nil?      ==false  ? slack_token   = opts[:token]      : abort("Must supply a token provoded by slack with '-t'! Want a token? Go here: https://api.slack.com/custom-integrations/legacy-tokens")
 opts[:outfolder].nil?  ==false  ? output_folder = opts[:outfolder]  : abort("Must supply an output folder name which will contain the json files for each users chat with you, use '-o' argument.")
 
+##### Getting all the users in a separate file
 `wget https://slack.com/api/users.list?token=#{slack_token} -O "users.list.json"`
 `cat users.list.json | tr , '\n' | grep -Po '"name":".*"' | sed 's/.*\":\"//g' | sed 's/"//g' > users_list.txt`
 
+##### Making the directory which will hold the json files
 if File.directory?(output_folder)
 	`rm -rf output_folder`
 	`mkdir #{output_folder}`
@@ -22,6 +24,7 @@ else
 	`mkdir #{output_folder}`
 end
 
+##### Creating an array with all the users
 users_list_file = File.open("users_list.txt")
 
 user_list = []
@@ -30,10 +33,7 @@ users_list_file.each do |line|
 	user_list.push(user)
 end
 
-#puts user_list.inspect
-user_list_split = user_list.each_slice(10).to_a
-#puts user_list_split.inspect
-
+##### Looping thorugh the array to generate json files for each user
 user_list.each do |each_user|
 	puts each_user.inspect
 	`slack-history-export --token #{slack_token} --username #{each_user} --filepath #{each_user}.json`
